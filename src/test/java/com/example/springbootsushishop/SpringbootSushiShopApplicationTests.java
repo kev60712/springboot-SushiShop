7e0eb12b1fcb48214054e464a4aa3110650bdf27
Kevin Liu <kev60712@gmail.com>
2022-10-29 10:56:43 +0800

    project init

---
+++ src/test/java/com/example/springbootsushishop/SpringbootSushiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsushishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSushiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
